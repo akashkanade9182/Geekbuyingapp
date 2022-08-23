19124c98357edb50502593e7e722499b8cb7129d
mdyajul <yajul2530@gmail.com>
2022-08-24 02:48:55 +0530

    navbar

---
+++ components/foter.js
@@ -0,0 +1,94 @@
+function foter(){
+    return `
+    <div id="top_foter">
+    <a href="#">Why buy from geekbuying?</a><p>+</p>
+  </div>
+  <div id="mid_foter">
+   <input type="text" id="search_foter" placeholder="Enter email to get a 5% off coupon">
+   <button>Subscribe</button>
+  </div>
+  <div id="down_list">
+    <div>
+    <h3>Help</h3>
+        <ul>Order status</ul>
+        <ul>About us</ul>
+        <ul>Contact us</ul>
+        <ul>Warranty</ul>
+        <ul>FAQs</ul>
+        <ul>Sitemap</ul>
+    </div>
+    <div>
+        <h3>Support</h3>
+        <ul>Payment information</ul>
+        <ul>Shipping guide</ul>
+        <ul> Wholesale</ul>
+        <ul> Drop shipping</ul>
+        <ul>Blog</ul>
+        <ul> Affiliates</ul>
+    </div>
+    <div>
+        <h3> Policies</h3>
+        <ul> Terms & conditions</ul>
+        <ul> Return policy</ul>
+        <ul> Privacy</ul>
+        <ul> Declaration</ul>
+        <ul> Customs</ul>
+        <ul>Sitemap</ul>
+    </div>
+    <div class="foter_social">
+        <h3>Download The App</h3>
+        <div class="scan">
+            <div><img src="https://content1.geekbuying.com/V1.4/en/images/index_images/android_app.png" alt=""></div>
+            <div>
+                <img src="https://content1.geekbuying.com/V1.4/en/images/index_images/app_store.jpg" alt="">
+                <img src="https://content1.geekbuying.com/V1.4/en/images/index_images/google_play.jpg" alt="">
+                <img src="https://content1.geekbuying.com/V1.4/en/images/index_images/gallery.jpg" alt="">
+            </div>
+        </div>
+        <div class="media">
+        <h3>Connect with us</h3>
+        <div>
+            <i class="fi fi-brands-facebook"></i>
+            <i class="fi fi-brands-youtube"></i>
+            <i class="fi fi-brands-telegram"></i>
+            <i class="fi fi-brands-instagram"></i>
+            <i class="fi fi-brands-twitter"></i>
+            <i class="fi fi-brands-tik-tok"></i>
+        </div>
+
+        </div>
+       
+    </div>
+    <div class="foter_chat">
+        <div><i class="fi fi-rr-envelope"></i><p>Service_payment@geekbuying.com</p></div>
+        <div><i class="fi fi-rr-ticket"></i><p>Ticket</p></div>
+        <div><i class="fi fi-rr-child-head"></i><p>Live Chat</p></div>
+    </div>
+  </div>
+  <div id="down_btn">
+    <p>Popular Searches</p>
+    <div>
+        <button>Geekbuying Coupon</button>
+        <button>Roborock Q7 Max</button>
+        <button>Tronsmart Bluetooth Speakers</button>
+        <button>Windows Mini PC</button>
+        <button>ENGWE X26</button>
+        <button>android tv boxes</button>
+        <button>KugooKirin G2 Pro</button>
+        <button>Mi Band 7 Pro</button>
+    </div>
+    <div>
+        <button>LDS Robot Vacuum</button>
+        <button>Electric Bike</button>
+    </div>
+
+  </div>
+
+  <div id="down_img"> 
+     <img src="https://content1.geekbuying.com/V1.4/en/images/indexV7/foot_icon.png" alt="lol"> 
+    
+  </div>
+    `
+}
+
+export default foter
---
+++ components/navbar.js
@@ -0,0 +1,32 @@
+function navbar(){
+    return `
+    <div id="top_nav">
+    <div>
+        <div><i class="fi fi-rs-mobile-button"></i><p> Save BIG with our app! </p><i class="fi fi-rr-angle-small-down"></i></div>
+        <div> <p>Language </p><i class="fi fi-rr-angle-small-down"></i></div>
+        <div><p>Support Centre</p><i class="fi fi-rr-angle-small-down"></i></div>
+    </div>   
+  </div>
+  <div id="mid_nav">
+       <div> <a href="#"><img src="https://content1.geekbuying.com/m/Content/images/headImg/geek_logo.png" alt="geek_logo"> </a></div>
+       <div> <div><p>All Categories</p><button><i class="fi fi-rr-caret-down"></i></button> <input type="text" id="search_input" placeholder="Search by keywords"><button><i class="fi fi-bs-search"></i></button></div></div>
+       <div>
+        <div><p>Ship to</p><img src="https://cdn-icons-png.flaticon.com/512/206/206606.png" alt=""> <p>/INR</p><i class="fi fi-rr-angle-small-down"></i></div>
+        <div><i class="fi fi-rs-user"></i><a href="#"><p>Sign in</p></a></div>
+        <div><a href="#"><i class="fi fi-bs-shopping-cart"></i></a><p class="cart">0</p></div>
+       </div>
+  </div>
+  <div id="down_nav">
+    <div><i class="fi fi-rr-menu-burger"></i><p>Categories</p></div>
+    <a href="#">New</a>
+    <a href="#">Bestselling</a>
+    <a href="#">Brand</a>
+    <a href="#">Clearance</a>
+    <a href="#">Deals</a>
+    <a href="#">Coupon</a>
+    <a href="#">App Only</a>
+  </div>
+    `
+}
+
+export default navbar
