4810f3ea297c3a9cd638a9be5569b80d74d6f6c9
mdyajul <yajul2530@gmail.com>
2022-08-26 02:06:58 +0530

    cart

---
+++ script/cheakout.js
@@ -0,0 +1,28 @@
+import navbar2 from "../components/navbar2.js";
+
+let nav2=document.getElementById("cart_nav")
+nav2.innerHTML=navbar2()
+
+import foter from "../components/foter.js";
+let fot=document.getElementById("foter")
+fot.innerHTML=foter()
+
+let save=document.getElementById("btn1")
+save.addEventListener("click",function(){
+let first=document.getElementById("first").value;
+    if(first===""){
+        alert ("Please fill the details")
+    }else{
+        alert ("Your Address Confirm")
+    }
+   
+    //window.location.href="navbar.html"
+})
+ 
+
+let cancel=document.getElementById("btn2")
+cancel.addEventListener("click",function(){
+    alert ("Reset Your Address")
+    window.location.reload()
+    //window.location.href="navbar.html"
+})
