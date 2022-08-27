4c9e1c6454312a0d977ba1788a7b29563f911594
mdyajul <yajul2530@gmail.com>
2022-08-27 18:30:24 +0530

    updated

--- components/navbar.js
+++ components/navbar.js
@@ -13,7 +13,7 @@ function navbar(){
        <div>
         <div><p>Ship to</p><img src="https://cdn-icons-png.flaticon.com/512/206/206606.png" alt=""> <p>/INR</p><i class="fi fi-rr-angle-small-down"></i></div>
         <div><i class="fi fi-rs-user"></i><a href="#"><p>Sign in</p></a></div>
-        <div><a href="#"><i class="fi fi-bs-shopping-cart"></i></a><p class="cart">0</p></div>
+        <div><a href="#"><i class="fi fi-bs-shopping-cart"></i></a><p id="cart_add">0</p></div>
        </div>
   </div>
   <div id="down_nav">
